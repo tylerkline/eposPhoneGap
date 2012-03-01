77787ac2f4ee7058b04c342c819f997fec5d83bc
Tyler Kline <tyler@emaginepos.com>
2012-03-01 00:23:01 -0500

    more

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,28 @@
+run(function () {
+	alert("here")
 //alert('here')
 //$("#my_device").html("Device: " + device.name + "<br/>Firmware Version: " + device.version)
-$("#my_device").html("dude")
+	$("#my_device").html("dude")
+});
 
 /*
-function handleLogin() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'http://tyler.emaginepos.com:5150/test.json');
-	xhr.onload = function (e) {
-		alert(this.response)
-		var data = JSON.parse(this.response);
-	}
-	xhr.send();
-}
+ function handleLogin() {
+ var xhr = new XMLHttpRequest();
+ xhr.open('GET', 'http://tyler.emaginepos.com:5150/test.json');
+ xhr.onload = function (e) {
+ alert(this.response)
+ var data = JSON.parse(this.response);
+ }
+ xhr.send();
+ }
 
-function setMenuScreen() {
-	x$("#my_device").html("Device: " + device.name + "<br/>Firmware Version: " + device.version)
-}
+ function setMenuScreen() {
+ x$("#my_device").html("Device: " + device.name + "<br/>Firmware Version: " + device.version)
+ }
 
-x$("#login_button").on("touchstart", function () {
-	handleLogin()
-})
+ x$("#login_button").on("touchstart", function () {
+ handleLogin()
+ })
 
-setMenuScreen()
-*/
+ setMenuScreen()
+ */
