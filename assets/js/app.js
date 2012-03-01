1124c6f8600a97223361c24848c6908c6c6ac993
Tyler Kline <tyler@emaginepos.com>
2012-02-29 22:39:37 -0500

    testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ run(function () {
 		 });*/
 	})();
 
-/*
+
 	// a little inline controller
 	x$("#login_button").on("touchstart", function () {
 		handleLogin()
@@ -30,7 +30,7 @@ run(function () {
 			var data = JSON.parse(this.response);
 		}
 		xhr.send();
-	}*/
+	}
 
 	function setMenuScreen() {
 		x$("#my_device").html("Device: " + device.name + "<br/>Firmware Version: " + device.version)
