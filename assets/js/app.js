055677e32c8c6b5aa96c6ce83631395e956fc395
Tyler Kline <tyler@emaginepos.com>
2012-03-01 10:38:48 -0500

    closer to login

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,44 @@
-$(document).ready(function(){
-	// login button tap
-	$("#login_button").tap(handleLogin )
-})
-document.addEventListener("deviceready", hookPhoneGapStuff, false);
+var documentReady = false
+var deviceReady = false
 
-function hookPhoneGapStuff() {
+// handle ready events for document and device  (phonegap)
+$(document).ready(handleDocumentReady)
+document.addEventListener("deviceready", handleDeviceReady, false);
+
+function handleDeviceReady() {
+	deviceReady = true
 	// hardware version
 	$("#my_device").html("Device: " + device.name + "<br/>Firmware Version: " + device.version)
 
 }
-function handleLogin() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'http://tyler.emaginepos.com:5150/test.json');
-	xhr.onload = function (e) {
-		alert(this.response)
-		var data = JSON.parse(this.response);
-	}
-	xhr.send();
+function handleDocumentReady() {
+	documentReady = true
+	// login button tap
+	$("#login_button").tap(handleLogin)
 
+}
+
+function handleLogin() {
+	// build json payload
+	var temp = {
+		Action:"Login",
+		Email:$("#login_email").value(),
+		Password:$("#login_password").value()
+	}
+
+	$.ajax({
+		type:'POST', // defaults to 'GET'
+		url:'http://192.168.1.31:5150/request', // defaults to window.location
+		data:temp, // can be a string, object or result of serializeArray()
+		dataType:'json', // what response type you accept from the server ('json', 'xml', 'html', or 'text')
+		async:true, // set async flag (true by default)
+		success:function (body) {
+			alert(body)
+		}, // body is a string (or if dataType is 'json', a parsed JSON object)
+		error:function (xhr, type) {
+			alert('error')
+		} // type is a string ('error' for HTTP errors, 'parsererror' for invalid JSON)
+	})
 }
 
 
