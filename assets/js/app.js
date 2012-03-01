5787cd585e4fb6edc485fee98d791ec164873fa0
Tyler Kline <tyler@emaginepos.com>
2012-03-01 11:18:04 -0500

    login with image

--- assets/js/app.js
+++ assets/js/app.js
@@ -62,6 +62,7 @@ function processLogin(account) {
 	$("#error_message").html("")
 	$("#account_info").html("")
 	$("<div></div>").html(account.Name).appendTo($("#account_info"))
+	$("<img/>").attr("src", "http://www.gravatar.com/avatar/" + account.EmailHash + "?d=mm&s=110&r=g").appendTo($("#account_info"))
 }
 function showError(message) {
 	$("#error_message").html(message)
